3cb69427af1707c3d7db930653990c340182c6d8
hrssilva <haroldo.rssilva@gmail.com>
2025-11-07 18:25:09 +0000

    Increased test run size and changed test env

--- large_run.sh
+++ large_run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-python stp_benchmarks.py --no-show --max-fw-n 800\
-    --dense-sizes 40 60 80 100 140 180 220 300 380 460 560 660 760 \
-    --sparse-sizes 40 60 80 100 140 180 220 300 380 460 560 660 760 
+python stp_benchmarks.py --no-show --max-fw-n 1200\
+    --repeats 5 \
+    --density-dense 0.95 \
+    --dense-sizes 40 60 80 100 140 180 220 300 380 460 560 660 760 900 1040 1180 \
+    --sparse-sizes 40 60 80 100 140 180 220 300 380 460 560 660 760 900 1040 1180
